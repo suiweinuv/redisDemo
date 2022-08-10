93d0986c78f9c74189658351c66d0e4c87fe491c
suiwei <136693118@qq.com>
2022-08-10 15:35:58 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# maven项目，非springboot
+
+# jedis客户端
+1.pom文件引入依赖
+2.建立连接（jedisTest.java）
+3.操作数据库（jedisTest.java）,方法名与redis命令一致
+4.释放资源
---
+++ src/test/java/com/suiwei/jedisTest.java
@@ -0,0 +1,51 @@
+package com.suiwei;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import redis.clients.jedis.Jedis;
+
+import java.util.Map;
+
+/**
+ * @author suiwei
+ * @date 2022-08-10 13:18
+ */
+
+public class jedisTest {
+    private Jedis jedis;
+
+    @BeforeEach//表示应在当前类中的每个@Test方法之前执行注解方法。
+    void setUp() {//setup主要实现测试前的初始化工作
+        jedis = new Jedis("124.70.29.109",6379);//建立连接
+        jedis.auth("Suiwei123!");//redis设置的密码
+        jedis.select(0);//选择数据库
+    }
+    @Test
+    void  test(){
+        String result = jedis.set("key01","val01");
+        System.out.println(result);
+        String result1 = jedis.get("key01");
+        System.out.println(result1);
+    }
+
+    @Test
+    void testhash() {
+        jedis.hset("key02","name","suiwei");
+        jedis.hset("key02","name","suiwei1");
+        jedis.hset("key02","age","12");
+        System.out.println(jedis.hget("key02","name"));
+        Map map =  jedis.hgetAll("key02");
+        System.out.println(map);
+
+    }
+
+    @AfterEach//表示应在当前类中的每个@Test方法之后执行注解方法
+    void tearDown() {
+        if (jedis!=null){
+            jedis.close();
+        }
+    }
+
+
+}
